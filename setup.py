7e052e6c40cc5e68ec4058e3ba2a2a8aa5edb3c6
akashsaingar <akash.s@permutehq.com>
2022-12-15 19:41:55 +0530

    dependency added in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='pdf-parser',
-    version='1.0',
-    packages=find_packages()
+    name="pdf-parser",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "python",
+        "PyDrive",
+        "dateparser",
+        "pdfminer.six",
+        "thefuzz",
+        "python-Levenshtein",
+        "openpyxl",
+        "lxml",
+        "PyPDF2",
+        "defusedxml",
+    ],
 )
